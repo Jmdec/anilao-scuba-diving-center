cf336e0ba3233a90f09bcd1752a9c56e7455f277
infinitecheirene <infinitech.eirene@gmail.com>
2026-06-03 13:28:03 +0800

    updated files

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,7 @@ const nextConfig = {
   },
   turbopack: {},
   async rewrites() {
-    const BACKEND_URL = process.env.BACKEND_URL || process.env.NEXT_PUBLIC_BACKEND_URL || "http://127.0.0.1:8000";
+    const BACKEND_URL = process.env.API_URL || process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
     return [
       {
         source: '/uploads/:path*',
